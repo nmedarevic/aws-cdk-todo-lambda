5e13417b942fe0eba8278b069a27342351bbd0d7
Nikola Medarevic <nikola@petmediagroup.com>
2024-03-16 12:02:30 +0100

    Separate db client and concrete db clients into separate modules

---
+++ backend/src/db/dynamoDbClient.ts
@@ -0,0 +1,28 @@
+import { DynamoDB } from 'aws-sdk';
+import { DBClient, DBClientDependencies } from "./dbClient";
+
+const table = "todo"
+
+export class DynamoDBClient extends DBClient {
+  private client;
+
+  constructor(private dependencies: DBClientDependencies) {
+    super(dependencies)
+
+    if (!this.dependencies.databaseUrl || !this.dependencies.region) {
+      throw new Error("Missing databaseUrl or databaseAccessKey")
+    }
+
+    this.client = new DynamoDB.DocumentClient({
+        region: this.dependencies.region,
+        endpoint: this.dependencies.databaseUrl,
+      });
+  }
+
+  async saveItem<T extends Object>(item: T) {
+      await this.client.put({
+        TableName: table,
+        Item: item
+      } as unknown as DynamoDB.Types.PutItemInput).promise()
+  }
+}
---
+++ backend/src/db/tursoDbClient.ts
@@ -0,0 +1,28 @@
+import { createClient } from "@libsql/client";
+import { DBClient, DBClientDependencies } from "./dbClient";
+
+export class TursoDBClient extends DBClient {
+  private client;
+
+  constructor(private dependencies: DBClientDependencies) {
+    super(dependencies)
+
+    if (!this.dependencies.databaseUrl || !this.dependencies.databaseAccessKey) {
+      throw new Error("Missing databaseUrl or databaseAccessKey")
+    }
+
+    this.client = createClient({
+      url: this.dependencies.databaseUrl,
+      authToken: this.dependencies.databaseAccessKey,
+    });
+  }
+
+  async saveItem<T extends Object>(item: T) {
+    const result = await this.client.execute({
+      sql: "INSERT INTO Todo (id, text, created_at, updated_at, user_id) VALUES (?, ?, ?, ?, ?)",
+      args: Object.values(item),
+    });
+
+    console.log(JSON.stringify(result));
+  }
+}
