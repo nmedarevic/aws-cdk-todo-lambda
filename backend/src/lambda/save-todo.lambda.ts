c316ec40d31e061b34a7e5b1a927060ed9a27323
Nikola Medarevic <nikola@petmediagroup.com>
2024-03-16 12:02:41 +0100

    Cleans up save-todo

--- backend/src/lambda/save-todo.lambda.ts
+++ backend/src/lambda/save-todo.lambda.ts
@@ -1,7 +1,8 @@
 import { APIGatewayProxyEvent, Context, APIGatewayProxyResult } from "aws-lambda"
-import { DynamoDB } from 'aws-sdk';
-import { createClient } from "@libsql/client";
+
 import { v4 as uuidv4 } from 'uuid';
+import { DBClient } from "../db/dbClient";
+import { TursoDBClient } from "../db/tursoDbClient";
 
 const getBody = (event: APIGatewayProxyEvent) => {
   try {
@@ -13,103 +14,32 @@ const getBody = (event: APIGatewayProxyEvent) => {
   }
 }
 
-const dbClient = new DynamoDB.DocumentClient({
-  region: 'eu-central-1',
-  endpoint: 'http://localstack:4566',
-})
-
-const table = "todos"
-
 type SaveTodoItemDTO = {
   id: string,
   text: string,
-}
-
-interface DBClientDependencies {
-  databaseUrl: string | undefined;
-  databaseAccessKey: string | undefined;
-}
-
-class DBClient {
-  private databaseUrl: string | undefined;
-  private databaseAccessKey: string | undefined;
-  
-  constructor(dependencies: DBClientDependencies) {
-    this.databaseUrl = dependencies.databaseUrl;
-    this.databaseAccessKey = dependencies.databaseAccessKey;
-  }
-
-  async saveItem(_: SaveTodoItemDTO) {
-    throw new Error("Not implemented")
-  }
-
-  async getItem() {
-    throw new Error("Not implemented")
-  }
-  
-  async updateItem() {
-    throw new Error("Not implemented")
-  }
-
-  async deleteItem() {
-    throw new Error("Not implemented")
-  }
-}
-
-class TursoDBClient extends DBClient {
-  private client;
-
-  constructor(private dependencies: DBClientDependencies) {
-    super(dependencies)
-
-    if (!this.dependencies.databaseUrl || !this.dependencies.databaseAccessKey) {
-      throw new Error("Missing databaseUrl or databaseAccessKey")
-    }
-console.log('\n\n', "URL", this.dependencies.databaseUrl, process.env, '\n\n');
-    this.client = createClient({
-      url: this.dependencies.databaseUrl,
-      authToken: this.dependencies.databaseAccessKey,
-    });
-  }
-
-  async saveItem(item: SaveTodoItemDTO) {
-    const result = await this.client.execute({
-      sql: "INSERT INTO Todo (id, text, created_at, updated_at, user_id) VALUES (?, ?, ?, ?, ?)",
-      args: [item.id, item.text, new Date(), new Date(), "user-id"],
-    });
-
-    console.log(JSON.stringify(result));
-  }
+  created_at: Date,
+  updated_at: Date,
+  user_id: string,
 }
 
 const tursoDBClient = new TursoDBClient({ databaseUrl: process.env.DATABASE_URL, databaseAccessKey: process.env.DATABASE_WRITE_ACCESS_KEY });
 
 const getLambdaHandler = ({ dbClient }: {dbClient: DBClient}) => {
   return async function handler(event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> {
-    console.log('\n\n', "EVENT", '\n\n');
-    console.log('\n\n', event.body, '\n\n');
-    console.log('\n\n', process.env, '\n\n');
-    // console.log('\n\n', process.env.DATABASE_URL, '\n\n');
-  
     const bodyJson = getBody(event)
     
-    const item = {
+    const item: SaveTodoItemDTO = {
       id: uuidv4(),
       text: bodyJson.text,
+      created_at: new Date(),
+      updated_at: new Date(),
+      user_id: bodyJson.user_id || uuidv4(),
     }
-  
-    console.log('\n\n', "Item", item, '\n\n');
   
     try {
       await dbClient.saveItem(item)
-
-      console.log('\n\n', "SAVED AN ITEM", '\n\n');
-      // await dbClient.put({
-      //   TableName: table,
-      //   Item: item
-      // } as DynamoDB.Types.PutItemInput).promise()
     } catch (error) {
-      console.log('\n\n', "Error", error, '\n\n');
+      console.log('\n\n', "Could not save a todo", error, '\n\n');
   
       return {
         statusCode: 500,
@@ -117,7 +47,7 @@ const getLambdaHandler = ({ dbClient }: {dbClient: DBClient}) => {
       }
     }
   
-    console.log('\n\n', "Finished inserting", '\n\n');
+    console.log('\n\n', "Todo inserted", '\n\n');
   
     return {
       statusCode: 200,
